1f53785afb8c0098eda04e880323e135feefcdc6
Mashud Murtaza <mashud@MacBook-Pro.local>
2022-02-19 19:49:16 +0500

    Updates

---
+++ precache-manifest.da51c4528ff9b2c8f925900b955edbb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba2b9b1ab2fb4f95d8c5f8c439e6f14",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b45a2d2cca3981b92bb4",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b98803b85267f56b4d27",
+    "url": "/portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "b45a2d2cca3981b92bb4",
+    "url": "/portfolio/static/js/2.e7223d30.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.e7223d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b98803b85267f56b4d27",
+    "url": "/portfolio/static/js/main.2ca46d77.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
